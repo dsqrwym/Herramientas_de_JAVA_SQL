c326c3e69fde0a0a9e653d13ed2037806c144f56
dsqrwym <dsqrwym@gmail.com>
2024-10-19 12:23:14 +0200

    Aniade funcion de ejecutar SQL script y comentarios·

--- mariadbSql/ConectarBD.java
+++ mariadbSql/ConectarBD.java
@@ -1,5 +1,6 @@
 package mariadbSql;
 
+import java.io.*;
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -15,7 +16,7 @@ public class ConectarBD {
     private static String user;
     private static String password;
 
-    private ConectarBD() {
+    private ConectarBD() { //evitar instanciacio
     }
 
     public static void setHost(String host) {
@@ -38,17 +39,23 @@ public class ConectarBD {
         ConectarBD.password = password;
     }
 
+    // Método para conectar a la base de datos
     public static boolean connectDatabase(boolean cerrar) {
         String url = "";
         boolean valida = false;
         try {
+            // Cargar el driver de MariaDB
             Class.forName("org.mariadb.jdbc.Driver");
+            // Crear la URL de conexión
             url = "jdbc:mariadb://" + host + ":" + port + "/" + database + "?user=" + user + "&password=" + password;
+            // Establecer la conexión
             connection = DriverManager.getConnection(url);
-            valida = connection.isValid(10);
+            valida = connection.isValid(10); // Verificar si la conexión es válida
             if (valida && cerrar) {
+                // Mostrar un mensaje de éxito si la conexión se ha creado correctamente
                 JOptionPane.showMessageDialog(null, "Conexion creada con exito");
             } else if (!valida) {
+                // Mostrar un mensaje si no se ha podido conectar
                 JOptionPane.showMessageDialog(null, "No se ha podido establecer conexion", "INFO",
                         JOptionPane.INFORMATION_MESSAGE);
             }
@@ -59,6 +66,7 @@ public class ConectarBD {
         } finally {
             if (cerrar) {
                 try {
+                    // Cerrar la conexión si se especifica
                     connection.close();
                 } catch (SQLException e) {
                     mensaje(e.getMessage());
@@ -68,6 +76,7 @@ public class ConectarBD {
         return valida;
     }
 
+    // Cerrar PreparedStatement si está abierto
     public static void cerrarPreparedStatement() {
         if (st != null) {
             try {
@@ -78,6 +87,7 @@ public class ConectarBD {
         }
     }
 
+    // Cerrar ResultSet si está abierto
     public static void cerrarResultSet() {
         if (rs != null) {
             try {
@@ -88,6 +98,7 @@ public class ConectarBD {
         }
     }
 
+    // Ejecutar una consulta SELECT en la base de datos
     public static ResultSet ejecutarSelect(String query, Object... valores) {
         boolean exito = (estaConectado() || connectDatabase(false));
 
@@ -96,6 +107,7 @@ public class ConectarBD {
         }
 
         try {
+            // Preparar la consulta con los valores dados
             st = connection.prepareStatement(query, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
 
             for (int i = 0; i < valores.length; i++) {
@@ -107,6 +119,7 @@ public class ConectarBD {
                 }
             }
 
+            // Ejecutar la consulta
             rs = st.executeQuery();
         } catch (SQLException e) {
             mensaje(e.getMessage());
@@ -115,11 +128,13 @@ public class ConectarBD {
         return rs;
     }
 
+    // Verificar si una consulta es DDL (CREATE, DROP, ALTER)
     public static boolean isDDL(String query) {
         String lowerCaseQuery = query.toLowerCase();
         return lowerCaseQuery.contains("create") || lowerCaseQuery.contains("drop") || lowerCaseQuery.contains("alter");
     }
 
+    // Verificar si un registro existe basado en la consulta dada
     public static boolean existe(String query, Object... parametros) {
         ResultSet resultSet = ejecutarSelect(query, parametros);
         try {
@@ -137,14 +152,17 @@ public class ConectarBD {
         return false;
     }
 
+    // Ejecutar una consulta DDL (CREATE, DROP, ALTER)
     public static boolean ejecutarDDL(String query, Object... valores) {
         return ejecutarQuery(query, true, valores);
     }
 
+    // Ejecutar una consulta DML (INSERT, UPDATE, DELETE)
     public static boolean ejecutarDML(String query, Object... valores) {
         return ejecutarQuery(query, false, valores);
     }
 
+    // Ejecutar una consulta genérica
     private static boolean ejecutarQuery(String query, boolean isDDL, Object... valores) {
         boolean exito = (estaConectado() || connectDatabase(false));
         boolean hecho = false;
@@ -177,6 +195,7 @@ public class ConectarBD {
         return hecho;
     }
 
+    // Cerrar la conexión a la base de datos
     public static void cerrarConnection() {
         try {
             if (connection != null && !connection.isClosed()) {
@@ -187,6 +206,7 @@ public class ConectarBD {
         }
     }
 
+    // Obtener información de una tabla basada en una consulta SELECT
     public static String obtenerInformacionEnFila(String queryDeConsulta, Object... valores) {
         StringBuilder contenidos = new StringBuilder();
 
@@ -262,23 +282,88 @@ public class ConectarBD {
         return contenidos.toString();
     }
 
+    // Verificar si hay conexión a la base de datos
     public static boolean estaConectado() {
-        if (connection != null) {
-            try {
-                return !connection.isClosed();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+        try {
+            return connection != null && !connection.isClosed();
+        } catch (SQLException e) {
+            return false;
         }
-        return false;
+    }
+
+    public static void ejecutarSqlScript(String archivo, String codificacion) {
+        if (archivo == null || archivo.isEmpty()) {
+            //Validar que el archivo no sea nulo o vacio
+            System.out.println("El archivo proporcionado es nulo o vacio.");
+            return;
+        }
+
+        // Verificar si hay conexión con la base de datos o conectarse
+        if (estaConectado() || connectDatabase(false)) {
+            //Usar try-with-resources para el BufferedReader y Statement se cierre automaticamente y adecuadamente
+            try (
+                    Statement statement = connection.createStatement();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(archivo), codificacion))
+            ) {
+                StringBuilder query = new StringBuilder();
+                String linea;
+                int contador = 0; //Para hacer referencia a cada las sentencias del SQL
+
+                while ((linea = reader.readLine()) != null) {
+                    //Eliminar espacios en blanco al principio y final de la linea
+                    linea = linea.trim();
+
+                    //Si la linea no esta vacio o no es un comentario
+                    if (!linea.isEmpty() && !linea.startsWith("--")){
+                        query.append(linea);
+                        //Si setencia SQL esta completa o no
+                        if (linea.endsWith(";")){
+                            //Aniade setencia y reinicial StringBuilder
+                            statement.addBatch(query.toString());
+                            query.setLength(0);
+                            contador++;
+                        }
+                    }
+                }
+
+                if (contador > 0) {
+                    int[] resultado = statement.executeBatch();
+
+                    for (int i = 0; i < resultado.length; i++){
+                        int resultadoSetencia = resultado[i];
+                        if (resultadoSetencia == Statement.SUCCESS_NO_INFO){
+                            System.out.printf("Setencia %d : Ejecutada con exito, pero se desconoce el numero de filas afectadas.%n", (i+1));
+                        } else if (resultadoSetencia == Statement.EXECUTE_FAILED) {
+                            System.out.printf("Setencia %d : Ejecucion fallada%n", (i+1));
+                        } else {
+                            System.out.printf("Setencia %d : Ejecutada con exito, afecto a %d filas.", (i+1), resultadoSetencia);
+                        }
+                    }
+                }else {
+                    System.out.println("No hay setencias SQL para ejecutar.");
+                }
+
+            } catch (SQLException e) {
+                System.err.println("Error ejecutando el script SQL -> " + e.getMessage());
+            } catch (FileNotFoundException e) {
+                System.err.printf("Archivo no encontrado -> %s%n%s%n", archivo, e.getMessage());
+            } catch (UnsupportedEncodingException e) {
+                System.err.printf("Codificación no soportada -> %s%n%s%n ", codificacion, e.getMessage());
+            } catch (IOException e) {
+                System.err.printf("Error de I/O leyendo el archivo -> %s%n", e.getMessage());
+            }
+        } else {
+            System.out.println("Base de datos desconectada!");
+        }
     }
 
     public static ArrayList<String> obtenerNombresTablas() {
-        ArrayList<String> nombresTablas = new ArrayList<>();
+        ArrayList<String> nombresTablas = new ArrayList<>();// Lista para almacenar los nombres de las tablas
         if (estaConectado() || connectDatabase(false)) {
             try {
                 DatabaseMetaData meta = connection.getMetaData();
-                rs = meta.getTables(null, null, "%", new String[] { "TABLE" });
+                rs = meta.getTables(null, null, "%", new String[]{"TABLE"});
+                // Añadir el nombre de cada tabla a la lista
                 while (rs.next()) {
                     nombresTablas.add(rs.getString("TABLE_NAME"));
                 }
@@ -295,6 +380,7 @@ public class ConectarBD {
         }
     }
 
+    // Mostrar un mensaje de error
     private static void mensaje(String string) {
         JScrollPane scrollPane = getScrollPane(string);
         JOptionPane optionPane = new JOptionPane(scrollPane, JOptionPane.INFORMATION_MESSAGE);
@@ -304,10 +390,11 @@ public class ConectarBD {
         dialog.setAlwaysOnTop(true); // Mantener el diálogo encima de otros
         // Mostrar el diálogo
         dialog.setVisible(true);
-        // JOptionPane.showInternalMessageDialog(null, string, "Error",
-        // JOptionPane.INFORMATION_MESSAGE);
+        // JOptionPane.showInternalMessageDialog(null, string, "Error",JOptionPane.INFORMATION_MESSAGE);
     }
 
+
+    // Crear un JScrollPane para mostrar mensajes en un cuadro de diálogo
     private static JScrollPane getScrollPane(String string) {
         JTextArea textArea = new JTextArea(string);
         textArea.setEditable(false); // El texto no se puede editar
